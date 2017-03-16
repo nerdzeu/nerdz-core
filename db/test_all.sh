3c5edd453a7342c14dc3ae278452e7938c8532ec
Marco Cilloni <marco.cilloni@yahoo.com>
2017-03-16 11:28:50 +0100

    New container based test_all.sh

--- db/test_all.sh
+++ db/test_all.sh
@@ -2,3 +2,13 @@
 
 # Test using the Docker database image
 
+if test -z "$DOCKER_IMG"
+then
+    export DOCKER_IMG="mcilloni/nerdz-test-db"
+fi
+
+CONT_NAME="nerdz-test-db"
+
+sudo docker run -d --rm --name "$CONT_NAME" -p 5432:5432 "$DOCKER_IMG" && \
+trap "sudo docker stop \"$CONT_NAME\"" INT TERM EXIT && \
+go test . 
